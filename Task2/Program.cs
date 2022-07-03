5c8267cbde0882be28a3d82c7b51984360e365dd
AndreyVolkov <volk_bur-gora@mail.ru>
2022-07-03 22:40:02 +0300

    Задание 2

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+
+int promt(string message)
+{
+    System.Console.WriteLine(message);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+
+int x1 = promt("введите х1");
+int y1 = promt("введите y1");
+int z1 = promt("введите z1");
+
+int x2 = promt("введите х2");
+int y2 = promt("введите y2");
+int z2 = promt("введите z2");
+
+int x = x2-x1;
+int y = y2-y1;
+int z = z2-z1;
+double l =  Math.Sqrt(x*x+y*y+z*z);
+
+System.Console.WriteLine(l);
